c940aeb5b7efc5459b9870436d9620826af0a945
Skanda112 <72615563+Skanda112@users.noreply.github.com>
2021-07-29 11:36:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 video = "";
 
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
 function setup()
 {
   canvas = createCanvas(400, 400);
@@ -7,6 +11,7 @@ function setup()
   canvas.center();
 
 	poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 function preload()
@@ -18,9 +23,30 @@ function preload()
 function draw()
 {
  image(video, 0, 0, 400, 400);
+
+ 
+ if(scoreRightWrist > 0.2)
+ {
+   fill("red");
+   stroke("red");
+   circle(rightWristX, rightWristY, 30);
+ }
 }
 
 function modelLoaded()
 {
   console.log("MODEL IS LOADED!");
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
 }
